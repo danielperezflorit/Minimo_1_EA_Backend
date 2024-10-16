a401259bf0d7b5a36d8802d90133c8f75061e264
Pedro <pedrojordan2001@gmail.com>
2024-10-16 21:35:25 +0200

    lo subo al main que es ultraimportante

--- src/modelos/types_d_users.ts
+++ src/modelos/types_d_users.ts
@@ -1,19 +1,17 @@
 import { model, Schema } from "mongoose";
 
 export interface usersInterface{
-    id: number,
     name: string,
     mail: string,
     password: string,
     comment: string,
     tipo: string
 }
-export type UsersInterfacePublicInfo = Pick<usersInterface, 'id' | 'name' | 'comment'>
+export type UsersInterfacePublicInfo = Pick<usersInterface,  'name' | 'comment'>
 export type UsersInterfacePrivateInfo = Pick<usersInterface, 'name' | 'password'>
 export type newUserInfo = Omit<usersInterface,'id'>
 
 export const usersSchema = new Schema<usersInterface>({
-    id: Number,
     name: String,
     mail: String,
     password: String,
